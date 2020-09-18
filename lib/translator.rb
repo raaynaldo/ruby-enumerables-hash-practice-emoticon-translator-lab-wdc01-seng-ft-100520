eb0c8d01df2e65d7a0f20289af555f7a5906d51a
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 20:04:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+
+def load_library(lib)
   # code goes here
+  emoticon = YAML.load_file(lib)
+  emoticon.each {|key, value|
+    emoticon[key] = {:english => value[0], :japanese => value[1]}
+  }
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(lib, emoticon)
   # code goes here
+  e = load_library(lib)
 end
 
 def get_english_meaning
   # code goes here
 end
+
+load_library('./lib/emoticons.yml')
